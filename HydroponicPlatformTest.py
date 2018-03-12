41f6b94b087f612c42d5f4b65f7bb7d5cefa3499
virtue244 <virtue2021@gmail.com>
2018-03-12 19:31:11 -0400

    Changes to tests

--- HydroponicPlatformTest.py
+++ HydroponicPlatformTest.py
@@ -1,9 +1,17 @@
 import unittest
+from HydroponicPlatform import HydroponicPlatform
 
 class HydroponicPlatformTest (unittest.TestCase):
 
-    def test_senseToTurnOnPumpCheck(self):
+    
+    def setUp(self):
         x = HydroponicPlatform(2, 30)
+    
+    def test_senseToTurnOnPumpCheck(self):
+        #x = HydroponicPlatform(2, 30)
         print x.senseToTurnOnPump(9) #returns 1
         print x.senseToTurnOnPump(11) #returns 0
         #self.assertEqual()
+
+if __name__ =='__main__':
+    unittest.main()
