620b52985379b45c5d07433a62053a86b6d71271
virtue244 <virtue2021@gmail.com>
2018-03-12 20:20:35 -0400

    Most recent tests
    test cases and according class

--- HydroponicPlatform.py
+++ HydroponicPlatform.py
@@ -1,20 +1,16 @@
 class HydroponicPlatform:
-
-    uvExposure = 0
-    fertInterval = 0
     
     def __init__(self, uv, fert):
         self.uvExposure = uv
         self.fertInterval = fert
 
-    def senseToTurnOnPump(self, moistureValue):
-        if moistureValue <= 10:
+    def senseToFlipPump(self, moistureValue):
+        if moistureValue <= 100:
             #Turn pump on or keep pump on
             return 1
         else:
             #shut off pump or keep pump off
             return 0
-        
 
     def flipTowerLight(self, curInput):
         #setting light pin output to 1
@@ -25,19 +21,32 @@ class HydroponicPlatform:
         else:
             return 2
 
+    def senseToAdjustLight(self, lightValue):
+        if lightValue >= 10:
+            return 0
+        elif lightValue < 10:
+            return 1
+        else:
+            return 2
+        
+    def checkForFert(self, daysPassed, fertInterval):
+        if daysPassed >= fertInterval:
+            return 1
+        elif daysPassed < fertInterval:
+            return 0
+        else:
+            return 2
+    
 
-x = HydroponicPlatform(2, 30)
-print x.uvExposure
-print x.fertInterval
 
-print "\nTest 1"
 
-#Test number 2.2.2 - 1
-print x.senseToTurnOnPump(9) #returns 1
-print x.senseToTurnOnPump(11) #returns 0
 
-print "\nTest 2"
 
-#Test number 2.2.2 - 2
-print x.flipTowerLight(1) #returns 0
-print x.flipTowerLight(0) #returns 1
+
+
+
+
+
+
+
+        
--- HydroponicPlatformTest.py
+++ HydroponicPlatformTest.py
@@ -2,16 +2,45 @@ import unittest
 from HydroponicPlatform import HydroponicPlatform
 
 class HydroponicPlatformTest (unittest.TestCase):
-
     
     def setUp(self):
-        x = HydroponicPlatform(2, 30)
+        # uvExposure set to an index of 10 
+        # fertInterval set to 30 days
+        self.x = HydroponicPlatform(10, 30)
+
+    #Test number 2.2.2 - 1
+    def test_senseToFlipPumpOn(self):
+        self.assertTrue(self.x.senseToFlipPump(99))
+
+    def test_senseToFlipPumpOff(self):
+        self.assertFalse(self.x.senseToFlipPump(101))
+
+    #Test number 2.2.2 - 2
+    def test_flipTowerLightOn(self):
+        self.assertTrue(self.x.flipTowerLight(0))
+
+    def test_flipTowerLightOff(self):
+        self.assertTrue(self.x.flipTowerLight(0))
+
+    #Test number 2.2.2 - 3
+    def test_senseToAdjustLightUp(self):
+        self.assertFalse(self.x.senseToAdjustLight(10))
+
+    def test_senseToAdjustLightDown(self):
+        self.assertTrue(self.x.senseToAdjustLight(3))
+
+    #Test number 2.2.2 - 4
+    def test_checkForFertPast(self):
+        self.assertTrue(self.x.checkForFert(30, self.x.fertInterval))
+
+    def test_checkForFertBefore(self):
+        self.assertFalse(self.x.checkForFert(29, self.x.fertInterval))
     
-    def test_senseToTurnOnPumpCheck(self):
-        #x = HydroponicPlatform(2, 30)
-        print x.senseToTurnOnPump(9) #returns 1
-        print x.senseToTurnOnPump(11) #returns 0
-        #self.assertEqual()
+    
+    
+    
+    
+    
 
 if __name__ =='__main__':
     unittest.main()
